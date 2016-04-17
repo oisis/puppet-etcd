8a328f0ca3d58dbd98c1cf4f24e77f88a5f5a183
OiSiS <oisis@o2.pl>
2016-04-17 22:26:20 +0200

    Added validation for service_ensure

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,11 @@
 # Class to manage the etcd service daemon
 #
 class etcd::service {
+
+  if ! ($::etcd::service_ensure in [ 'running', 'stopped' ]) {
+    fail('service_ensure parameter must be running or stopped')
+  }
+
   if $::etcd::manage_service {
     service { 'etcd':
       ensure => $::etcd::service_ensure,
