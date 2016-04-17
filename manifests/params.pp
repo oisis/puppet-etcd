f3f020aeb65ca3f0ece4468216166f51ad6e1d9a
OiSiS <oisis@o2.pl>
2016-04-17 20:25:40 +0200

    Define vars to undef - daemon is running with default params anyway

--- manifests/params.pp
+++ manifests/params.pp
@@ -27,42 +27,42 @@ class etcd::params {
   $service_enable = true
   # member options
   $wal_dir = ''
-  $snapshot_counter = 10000
-  $heartbeat_interval = 100
-  $election_timeout = 1000
-  $listen_client_urls = ['http://localhost:2379', 'http://localhost:4001']
-  $advertise_client_urls = ['http://localhost:2379', 'http://localhost:4001']
-  $max_snapshots = 5
-  $max_wals = 5
+  $snapshot_counter = undef
+  $heartbeat_interval = undef
+  $election_timeout = undef
+  $listen_client_urls = ['http://0.0.0.0:2379']
+  $advertise_client_urls = ['http://localhost:2379']
+  $max_snapshots = undef
+  $max_wals = undef
   $cors = undef
 
   # cluster options
-  $listen_peer_urls = ['http://localhost:2380', 'http://localhost:7001']
+  $listen_peer_urls = ['http://localhost:2380', 'http://localhost:4001']
   $initial_advertise_peer_urls = ['http://localhost:2380']
-  $initial_cluster_state = 'new'
-  $initial_cluster_token = 'etcd-cluster'
+  $initial_cluster_state = undef
+  $initial_cluster_token = undef
   $discovery = undef
   $discovery_srv = undef
-  $discovery_fallback = 'proxy'
+  $discovery_fallback = undef
   $discovery_proxy = undef
-  $strict_reconfig_check = false
+  $strict_reconfig_check = undef
 
   # proxy
   $proxy = undef
-  $proxy_failure_wait = 5000
-  $proxy_refresh_interval = 30000
-  $proxy_dial_timeout = 1000
-  $proxy_write_timeout = 5000
-  $proxy_read_timeout = 0
+  $proxy_failure_wait = undef
+  $proxy_refresh_interval = undef
+  $proxy_dial_timeout = undef
+  $proxy_write_timeout = undef
+  $proxy_read_timeout = undef
 
   # security
   $cert_file = undef
   $key_file = undef
-  $client_cert_auth = false
+  $client_cert_auth = undef
   $trusted_ca_file = undef
   $peer_cert_file = undef
   $peer_key_file = undef
-  $peer_client_cert_auth = false
+  $peer_client_cert_auth = undef
   $peer_trusted_ca_file = undef
 
   # logging
