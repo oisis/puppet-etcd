cd68a44a5e3374f6cf030f103254c0c7900f30d0
OiSiS <oisis@o2.pl>
2016-04-18 22:29:07 +0200

    Added params description, reassigned vars

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,30 @@
 # == Class: etcd::config
 #
-class etcd::config {
-  file { $::etcd::config_file_path:
+# This module configure etcd service
+#
+# Parameters:
+#
+# [*etcd_packagename*]
+#   Define rpm/deb package name to install etcd
+#   Defaults to etcd
+#
+# [*config_file_path*]
+#   Where the config file should be put
+#   Defaults to running
+#
+class etcd::config(
+    $package_name     = $::etcd::etcd_packagename,
+    $config_file_path = $::etcd::config_file_path,
+  ) inherits etcd {
+  validate_string($package_name)
+  validate_absolute_path($config_file_path)
+
+  file { $config_file_path:
     notify  => Service['etcd'],
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
     content => template("${module_name}/etc/etcd/etcd.conf.erb"),
-    require => Package[ $::etcd::etcd_packagename ],
+    require => Package[ $package_name ],
   }
 }
