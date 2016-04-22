e4e3bdf2d03589b93f1528f1ad5be3674cf19cf6
Krzysztof Dziurawiec <krzysztof.dziurawiec@hrs.com>
2016-04-22 16:53:34 +0200

    Rewrite spec test for etcd class

--- spec/classes/etcd_spec.rb
+++ spec/classes/etcd_spec.rb
@@ -1,17 +1,55 @@
 require 'spec_helper'
 
-describe 'etcd' do
-  context 'with defaults for all parameters on RedHat' do
-    let :facts do
-      {
-        :kernel   => 'Linux',
-        :osfamily => 'RedHat',
-        :operatingsystemmajrelease => '7',
-        :fqdn => 'my_hostname.domain.net',
-        :ipaddress => '1.2.3.4',
+describe 'etcd', :type => :class do
+  describe 'On an unknown OS' do
+    let(:facts) { {:osfamily => 'Unknown'} }
+    it { should raise_error() }
+  end
+
+  describe 'On a Debian OS' do
+    let(:facts) { {
+        :osfamily => 'Debian',
+        :fqdn     => 'etcd.test.local'
+      } }
+
+    context 'When overriding service parameters' do
+      let(:params) { {
+          :service_ensure => 'stopped',
+          :service_enable => false
+        } }
+      it { should contain_service('etcd').with_ensure('stopped').with_enable('false') }
+    end
+
+    context 'When specifying a custom discovery endpoint and token' do
+      let(:params) { {
+          :discovery          => true,
+          :discovery_endpoint => 'http://local-discovery:4001/v2/keys/',
+          :discovery_token    => '123456'
+        } }
+      it {
+        should contain_file('/etc/etcd/etcd.conf').with_content(/discovery\s*= "http:\/\/local-discovery:4001\/v2\/keys\/123456"/)
       }
     end
 
-    it { should contain_class('etcd') }
+    context 'When enabling cluster discovery without a token' do
+      let(:params) { { :discovery => true } }
+      it {
+        should raise_error()
+      }
+    end
   end
+
+  describe 'On a Redhat OS' do
+    let(:facts) { {
+        :osfamily => 'Redhat',
+        :fqdn     => 'etcd.test.local'
+      } }
+
+    context 'When overriding service parameters' do
+      let(:params) { {
+          :service_ensure => 'stopped',
+          :service_enable => false }}
+      it { should contain_service('etcd').with_ensure('stopped').with_enable('false') }
+    end
+
 end
